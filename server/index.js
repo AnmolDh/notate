6017586eb81ff14ad0bd6fb4c3646d21f1bb32b1
AnmolDh <73706789+AnmolDh@users.noreply.github.com>
2023-07-08 22:43:03 +0530

    api route

--- server/index.js
+++ server/index.js
@@ -16,6 +16,28 @@ const notesSchema = new mongoose.Schema({
 
 const Note = mongoose.model("Note", notesSchema);
 
+app.get("/", (req, res) => {
+  Note.find({}).then((data) => {
+    res.send(data);
+  })
+});
 
+app.post("/", (req, res) => {
+  const reqTitle = req.body.title;
+  const reqContent = req.body.content;
+  const note = new Note({
+    title: reqTitle,
+    content: reqContent
+  });
+  note.save();
+  res.send(note);
+});
+
+app.delete("/", (req, res) => {
+  const reqDelete = req.body.id;
+  Note.deleteOne({ _id: reqDelete })
+    .then((resp) => res.send(resp))
+    .catch((err) => res.send(err));
+});
 
 app.listen(4000, () => console.log("server started!"));
