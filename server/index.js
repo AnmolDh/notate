6af69af09a2280758748c740d22dbc910f413e79
AnmolDh <73706789+AnmolDh@users.noreply.github.com>
2023-07-08 22:42:44 +0530

    mongoDB schema

--- server/index.js
+++ server/index.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+mongoose.connect("mongodb://localhost:27017/notesDB");
+
+const notesSchema = new mongoose.Schema({
+  title: String,
+  content: String
+});
+
+const Note = mongoose.model("Note", notesSchema);
+
+
 
 app.listen(4000, () => console.log("server started!"));
