12de6652cb8e807fa3d19b1c79a441e6c39078e1
AnmolDh <73706789+AnmolDh@users.noreply.github.com>
2023-07-08 23:16:28 +0530

    restful

---
+++ server/app.js
@@ -0,0 +1,66 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+mongoose.connect("mongodb://localhost:27017/notesDB");
+
+const notesSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+});
+
+const Note = mongoose.model("Note", notesSchema);
+
+app.get("/", (req, res) => {
+  Note.find({}).then((data) => {
+    res.send(data);
+  });
+});
+
+app.post("/", (req, res) => {
+  const note = new Note({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  note.save();
+  res.send(note);
+});
+
+app.delete("/", (req, res) => {
+  Note.deleteOne({ _id: req.body.id })
+    .then(() => res.send("object deleted"))
+    .catch((err) => res.send(err));
+});
+
+app.put("/", (req, res) => {
+  Note.replaceOne(
+    {
+      _id: req.body.id,
+    },
+    {
+      title: req.body.title,
+      content: req.body.content,
+    }
+  )
+    .then((r) => res.send(r))
+    .catch((err) => res.send(err));
+});
+
+app.patch("/", (req, res) => {
+  Note.updateOne(
+    { _id: req.body.id },
+    {
+      title: req.body.title,
+      content: req.body.content,
+    }
+  )
+    .then((r) => res.send(r))
+    .catch((err) => res.send(err));
+});
+
+app.listen(4000, () => console.log("server started!"));
