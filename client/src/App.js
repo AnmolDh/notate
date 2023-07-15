2fcf8054919e8f6df4450cb3c86a5182c8b689d8
AnmolDh <73706789+AnmolDh@users.noreply.github.com>
2023-07-15 11:31:45 +0530

    router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 
 function App() {
   return (
-    <Login />
+    <Routes>
+      <Route path="/" element={<Home/> } />
+      <Route path="/login" element={<Login/>} />
+    </Routes>
   );
 }
 
