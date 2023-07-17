c761ccf02049a26c5ccbdbebccfd84035340603f
AnmolDh <73706789+AnmolDh@users.noreply.github.com>
2023-07-17 11:53:07 +0530

    logout route

--- server/routes/authHandler.js
+++ server/routes/authHandler.js
@@ -10,13 +10,26 @@ app.get(
 app.get(
   "/auth/google/callback",
   passport.authenticate("google", {
-    failureRedirect: `${process.env.CLIENT_URL}http://localhost:3000/login`,
+    failureRedirect: `${process.env.CLIENT_URL}`,
   }),
   function (req, res) {
-    res.redirect(process.env.CLIENT_URL);
+    res.redirect(`${process.env.CLIENT_URL}/home`);
   }
 );
 
 app.get("/isAuth", (req, res) => res.send(req.isAuthenticated()));
+
+app.get("/logout", (req, res) => {
+  if (req.isAuthenticated()) {
+    req.logout((err) => {
+      if (err) {
+        res.json({ logout: false });
+      }
+      res.json({ logout: true });
+    });
+  } else {
+    res.status(401).json({ error: "Unauthorized Access" });
+  }
+});
 
 module.exports = app;
